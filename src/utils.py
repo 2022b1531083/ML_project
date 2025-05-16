2cf612161a524cee3a26b21c885e69d769199e63
Yogesh Kumar <yogiafse@gmail.com>
2025-05-16 16:06:20 +0530

    adding overall pipeline

---
+++ src/utils.py
@@ -0,0 +1,53 @@
+import os
+import sys
+import pickle
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+from src.exception import CustomException
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
+    try:
+        report = {}
+        best_fitted_models = {}
+
+        for model_name, model in models.items():
+            params = param.get(model_name, {})
+
+            gs = GridSearchCV(model, params, cv=3)
+            gs.fit(X_train, y_train)
+
+            best_model = gs.best_estimator_
+
+            y_train_pred = best_model.predict(X_train)
+            y_test_pred = best_model.predict(X_test)
+
+            train_score = r2_score(y_train, y_train_pred)
+            test_score = r2_score(y_test, y_test_pred)
+
+            report[model_name] = test_score
+            best_fitted_models[model_name] = best_model  # <-- store fitted model
+
+        return report, best_fitted_models
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
